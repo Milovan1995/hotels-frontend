64745994dcfe440ddcd64563ba7b318ed621c5a7
Milovan <milovan.antic314@gmail.com>
2023-12-29 19:51:03 +0100

    Aditional changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { HotelsComponent } from './components/pages/hotels/hotels.component';
 import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HotelsComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
