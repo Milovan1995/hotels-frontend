02b7db5c1c627c39ca95333700aaf52ad928fee1
Milovan <milovan.antic314@gmail.com>
2023-12-29 19:06:06 +0100

    Initial fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { HotelsComponent } from './components/pages/hotels/hotels.component';
+import { ContactUsComponent } from './components/pages/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HotelsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
