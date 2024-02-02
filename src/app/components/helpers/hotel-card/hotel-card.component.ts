0e345cdc4c450fbbb7ebd11ad7d3714349767516
Milovan <milovan.antic314@gmail.com>
2024-02-03 00:25:14 +0100

    Remove delete and edit buttons from hotel cards for non admins

--- src/app/components/helpers/hotel-card/hotel-card.component.ts
+++ src/app/components/helpers/hotel-card/hotel-card.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Hotel } from '../../../models/Hotel';
 import { HotelService } from '../../../services/hotel.service';
 import { environment } from '../../../../environments/environment.development';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-hotel-card',
@@ -13,8 +14,12 @@ export class HotelCardComponent {
   @Output() deleteClicked: EventEmitter<Hotel> = new EventEmitter();
   apiUrl = environment.API_URL;
 
-  constructor(private hotelService: HotelService) {}
+  constructor(
+    private hotelService: HotelService,
+    private authService: AuthService
+  ) {}
 
+  userIsAdmin: boolean = this.authService.getUserData().isAdmin;
   onClickDelete() {
     this.deleteClicked.emit(this.hotel);
   }
